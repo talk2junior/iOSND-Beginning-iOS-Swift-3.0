216aa96473765a6c2b2287d89dc038a689fd89b9
talk2junior <talk2junior@gmail.com>
2017-01-06 11:21:23 -0500

    Functions I  exercise 1 to 3

--- Playground Collection/Part 2 - Robot Maze 2/Functions 2/Functions 2.playground/Pages/Overloading Functions Practice.xcplaygroundpage/Contents.swift
+++ Playground Collection/Part 2 - Robot Maze 2/Functions 2/Functions 2.playground/Pages/Overloading Functions Practice.xcplaygroundpage/Contents.swift
@@ -9,10 +9,17 @@
 //: ### Exercise 2
 //: Define a function called `volume` that takes 2 `Double` parameters: `radius` and `height`. The function should return a `Double` that is the volume of a cylinder which is roughly `3.14 * r * r * h`.
 //: **The solution is available on the next page!**
+func volume(radius: Double, height: Double) -> Double {
+    return (3.14 * radius * radius * height)
+}
 
+volume(radius: 3,height: 5)
 //: ### Exercise 3
 //: Define a function called `volume` that takes 3 `Double` parameters: `length`, `height`, and `width`. The function should return a `Double` that is the volume of a cuboid which is `length * height * width`.
 //: **The solution is available on the next page!**
+func volume(length: Double, height: Double, width: Double) -> Double {
+    return length * height * width
+}
 
-
+volume(length: 4, height: 7, width: 9)
 //: [Next](@next)
