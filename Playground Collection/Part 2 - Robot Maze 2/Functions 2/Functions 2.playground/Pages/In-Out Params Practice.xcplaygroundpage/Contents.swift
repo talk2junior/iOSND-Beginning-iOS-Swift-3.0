23d2acfc1fccbfa8fd6f6c315ba152148543fb19
talk2junior <talk2junior@gmail.com>
2017-01-06 12:32:05 -0500

    Completed Tuples and In-Out Parameters lesson

--- Playground Collection/Part 2 - Robot Maze 2/Functions 2/Functions 2.playground/Pages/In-Out Params Practice.xcplaygroundpage/Contents.swift
+++ Playground Collection/Part 2 - Robot Maze 2/Functions 2/Functions 2.playground/Pages/In-Out Params Practice.xcplaygroundpage/Contents.swift
@@ -7,5 +7,13 @@ struct EmployeeData {
 //: ### Exercise 1
 //: Define a function called `increasePay` that takes 1 `EmployeeData` parameter called `employeeData` and 1 `Double` parameter called `percentRaise`. **`employeeData` should be a inout parameter**. The function should **add** to its `baseSalary` property by a value of `baseSalary * percentRaise` and increase the `raisesGiven` property by 1.
 //: **The solution is available on the next page!**
+func increasePay(employeeData: inout EmployeeData, percentRaise: Double) {
+    employeeData.baseSalary += (employeeData.baseSalary * percentRaise)
+    employeeData.baseSalary += 1
+}
+
+var employee001 = EmployeeData(baseSalary: 50000, raisesGiven: 5)
+employee001.raisesGiven
+employee001.baseSalary
 
 //: [Next](@next)
--- Playground Collection/Part 2 - Robot Maze 2/Functions 2/Functions 2.playground/Pages/Tuples Practice.xcplaygroundpage/Contents.swift
+++ Playground Collection/Part 2 - Robot Maze 2/Functions 2/Functions 2.playground/Pages/Tuples Practice.xcplaygroundpage/Contents.swift
@@ -5,5 +5,15 @@
 //: ### Exercise 1
 //: Define a function called `basicOperations` that takes 2 `Double` parameters: `x` and `y`. The function should return a tuple of type `(Double, Double, Double, Double)` that contains resulting values for each of the four basic mathematical operations in this order: addition (+), subtraction (-), multiplication (*), and division (/). Each operation should be applied as `x [operator] y`. **Using zero may result in a crash!**.
 //: **The solution is available on the next page!**
+func baseOperations(x: Double, y: Double) -> (Double, Double, Double, Double) {
+    return (x + y, x - y, x * y, x / y)
+}
+
+let results = baseOperations(x: 5, y: 4)
+results.0 // this would be index 0 from the tuple within the baseOperations function
+results.1 // this would be index 1 from the tuple within the baseOperations function
+results.2 // this would be index 2 from the tuple within the baseOperations function
+results.3 // this would be index 3 from the tuple within the baseOperations function
+
 
 //: [Next](@next)
