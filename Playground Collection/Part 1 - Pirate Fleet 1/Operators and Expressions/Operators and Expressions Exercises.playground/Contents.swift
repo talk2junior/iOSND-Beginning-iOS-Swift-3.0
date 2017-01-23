4d7ad92a9b240219a1723ce8e0b89249d846a7dd
Americo Garcia Jr <talk2junior@gmail.com>
2017-01-23 17:51:52 -0500

    More exercises

--- Playground Collection/Part 1 - Pirate Fleet 1/Operators and Expressions/Operators and Expressions Exercises.playground/Contents.swift
+++ Playground Collection/Part 1 - Pirate Fleet 1/Operators and Expressions/Operators and Expressions Exercises.playground/Contents.swift
@@ -11,32 +11,38 @@ let twelve = 12
 let twenty = 20
 
 // Desired Value: 4
+twenty/five
 // Desired Value: -8
+twelve-twenty
 // Desired Value: 0
+twenty%five
 // Desired Value: 100
+twenty*five
 // Desired Value: 10
-
+((twenty-twelve)*five)/(twenty/five)
 /*:
 ### Exercise 2
 
 Replace the variable "replaceMe" in the following expressions so that the expressions evaluate to the desired value.
 
 */
-var replaceMe = 1
+var replaceMe = 16
+var replaceMe2 = 8
+var replaceMe3 = 24
 
 replaceMe % 10          // Desired Value: 6
-replaceMe * replaceMe   // Desired Value: 64
-5 * (replaceMe - 3)     // Desired Value: 105
+replaceMe2 * replaceMe2   // Desired Value: 64
+5 * (replaceMe3 - 3)     // Desired Value: 105
 
 /*:
 ### Exercise 3
 
 Use parentheses to modify the following expressions so that they evaluate to the desired value.
 */
-36 / 3 + 1              // Desired Value: 9
-2 * 5 + 3 * 2 + 4       // Desired Value: 28
-2 * 5 + 3 * 2 + 4       // Desired Value: 96
-144 / 12 - 8 * 1 + 16   // Desired Value: 7
+36 / (3 + 1)              // Desired Value: 9
+(2 * 5) + (3 * (2 + 4))       // Desired Value: 28
+(2 * (5 + 3) * (2 + 4))       // Desired Value: 96
+144 / (12 - (8 * 1) + 16)   // Desired Value: 7
 
 
 
